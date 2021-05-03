e517be1adae034a0395d49cf816a509f6637e12d
megan <msmeganmcmanus@gmail.com>
2021-05-02 17:01:23 -0700

    complete sorting ex

--- 10a_SortingEx/bubble.js
+++ 10a_SortingEx/bubble.js
@@ -1,3 +1,19 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let swap;
+		for (let j = 0; j < arr.length - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let holder = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = holder;
+				swap = true;
+			}
+		}
+
+		if (!swap) break;
+	}
+
+	return arr;
+}
 
 module.exports = bubbleSort;
--- 10a_SortingEx/quick.js
+++ 10a_SortingEx/quick.js
@@ -3,12 +3,34 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start = 0, end = arr.length - 1) {
+	const swap = (arr, idx1, idx2) => {
+		[ arr[idx1], arr[idx2] ] = [ arr[idx2], arr[idx1] ];
+	};
 
-/*
-quickSort accepts an array, left index, and right index
-*/
+	// We are assuming the pivot is always the first element
+	let pivot = arr[start];
+	let swapIdx = start;
 
-function quickSort() {}
+	for (let i = start + 1; i <= end; i++) {
+		if (pivot > arr[i]) {
+			swapIdx++;
+			swap(arr, swapIdx, i);
+		}
+	}
 
-module.exports = quickSort;
+	// Swap the pivot from the start the swapPoint
+	swap(arr, start, swapIdx);
+	return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+	if (left < right) {
+		let pivotIndex = pivot(arr, left, right);
+		quickSort(arr, left, pivotIndex - 1);
+		quickSort(arr, pivotIndex + 1, right);
+	}
+	return arr;
+}
+
+module.exports = { pivot, quickSort };
